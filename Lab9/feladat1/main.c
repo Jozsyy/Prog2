d241be34ef2e9ee2ab45b5972ad08bfbcfbfab12
Jozsi <miklo.jozsef.peter@student.ms.sapientia.ro>
2021-11-17 10:43:31 +0000

    feladatok1_complete

--- Lab9/feladat1/main.c
+++ Lab9/feladat1/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-//void dfs(char**,int**,int,int,int);
+void dfs(char**,int**,int*,int*,int,int,int);
 
 int main() {
     FILE *f=fopen("bemenet.txt","r");
@@ -22,14 +22,13 @@ int main() {
         adjM[i]=(int*)calloc(n+1,sizeof(int));
     }
 
-    for(int i=1;i<=n;++i){
-        printf("%s\n",s[i]);
-    }
+//    for(int i=1;i<=n;++i){
+//        printf("%s\n",s[i]);
+//    }
 
     for(int i=0;i<m;++i){
         int el1,el2;
         fscanf(f,"%i%i\n",&el1,&el2);
-        printf("%i%i",el1,el2);
         adjM[el1][el2]=1;
         adjM[el2][el1]=1;
     }
@@ -45,31 +44,40 @@ int main() {
     ///beolvassuk a 2 nevet
     char x[30],y[30];
     int x1,y1;
-    fscanf(f, "%[^\n]\n%[^\n]\n", x,y);
+    fscanf(f, "%[^\n]\n %[^\n]\n", x,y);
+    fclose(f);
     ///megkeressuk, hogy hanyadik csomopontok a keresett szemelyek
     for(int i=0;i<n;++i){
         //kezdo csomopont x1
-        if(strcmp(strstr(s[i],x),x)==0){
+        if(strcmp(s[i],x)==0){
             x1=i;
-            printf("%i\t",x1);
         }
         //vegso csomopont y1
-        if(strcmp(strstr(s[i],y),y)==0){
+        if(strcmp(s[i],y)==0){
             y1=i;
-            printf("%i\t",y1);
         }
     }
 
-    int k=0;//szamlalo
-    for(int i=x1;i<=y1;++i){
-        color[i]=1;
-        tav[k]=i;
-
-    }
-
+    printf("%s es %s tavolsaga melysegi bejarassal:",x,y);
+    dfs(s,adjM,color,tav,x1,y1,n);
     return 0;
 }
 
-void dfs(char **s,int **adjM,int n,int x1,int y1){
-
+void dfs(char **s,int **adjM,int *color,int *tav,int x,int y,int n){
+    int j;
+    color[x]=1;
+    //printf("%i->",x);
+    for(j=1;j<=n;++j){
+        if(adjM[x][j]==1 && color[j]==0){
+            int k=0;
+            while(tav[k]!=0){++k;}
+            tav[k]=j;
+            if(j==y){
+                printf("%i",k);
+                break;
+            }
+            dfs(s,adjM,color,tav,j,y,n);
+        }
+    }
+    color[x]=2;
 }
