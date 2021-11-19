5522cabe683d8a4f678cdedace6185c322e84bc7
Jozsi <miklo.jozsef.peter@student.ms.sapientia.ro>
2021-11-19 17:37:47 +0000

    feladat2_dfs

--- Lab9/feladat2/main.c
+++ Lab9/feladat2/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void dfs(int**, int*, int*, char*, char*, char*, int, int);
 
 int main() {
     FILE *f=fopen("bemenet.txt","r");
@@ -7,14 +10,22 @@ int main() {
         printf("Cannot open the input file!");
         return 0;
     }
-    int n,m,**adjM;
-    char *s;
+    int n,m,**adjM,*color,*dist,x;
+    char *s,*test,*dists;
     fscanf(f,"%i%i\n",&n,&m);
+
+    //Helyfoglalasok
     adjM=(int**)calloc(n+1,sizeof(int*));
     for(int i=1;i<=n;++i){
         adjM[i]=(int*)calloc(n+1,sizeof (int));
     }
+    color=(int*)calloc(n+1,sizeof(int));
+    dist=(int*)calloc(n+1,sizeof(int));
     s=(char*)malloc((n+1)*sizeof(char));
+    test=(char*)malloc((n+1)*sizeof(char));
+    dists=(char*)calloc((n+1),sizeof(char));
+
+    //beolvasasok
     char aux;//szokozoket ide olvassuk
     for(int i=1;i<=n;++i){
         fscanf(f,"%c%c",&s[i],&aux);
@@ -26,11 +37,40 @@ int main() {
         adjM[el2][el1]=1;
     }
 
+    fscanf(f,"%s",test);
+    fscanf(f,"%i",&x);
+    dist[0]=x;
+    dists[0]=s[x];
+    fclose(f);
+
     for(int i=1;i<=n;++i){
         for(int j=1;j<=n;++j){
             printf("%i ",adjM[i][j]);
         }
         printf("\n");
     }
+
+    dfs(adjM,color, dist,s,test, dists,n,x);
     return 0;
+}
+
+void dfs(int **adjM, int *color, int *dist, char *s, char *test, char* dists, int n,int x){     ///dist-melysegi bejaras x-bol indulva  ///dists- melysegi bejaras xbol karakterlanckent
+    int j;
+    color[x]=1;
+    //printf("%i->",x);
+    for(j=1;j<=n;++j){
+        if(adjM[x][j]==1 && color[j]==0){
+            int k=0;
+            while(dist[k]!=0){++k;}
+            dist[k]=j;
+            dists[k]=s[j];
+            if(strlen(dists)>=strlen(test) && strstr(dists,test)!=NULL && strcmp(strstr(dists,test),test)==0){
+                printf("\nSikeresen megkaptuk a teszt:%s karakterlancot a melysegi bejarasssal",test);
+                printf("\nAz eddigi megtett tavolsag karakterlancokban:%s",dists);
+                break;
+            }
+            dfs(adjM,color,dist,s,test,dists,n,j);
+        }
+    }
+    color[x]=2;
 }
