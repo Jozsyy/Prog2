c2fe234b9baac4bcfcc1b3ba372590582501b2d3
Jozsi <miklo.jozsef.peter@student.ms.sapientia.ro>
2021-12-14 17:08:47 +0000

    filmek-Greedy

--- Lab12/Filmek_Greedy/main.c
+++ Lab12/Filmek_Greedy/main.c
@@ -20,11 +20,40 @@ int main() {
         fscanf(f,"%[^\n]\n",a[i].cim);
         fscanf(f,"%i%i\n",&a[i].kezd,&a[i].veg);
     }
+    fclose(f);
 
-    for(int i=0;i<n;++i){
-        printf("%s:",a[i].cim);
-        printf("%i %i\n",a[i].kezd,a[i].veg);
+//    for(int i=0;i<n;++i){
+//        printf("%s:",a[i].cim);
+//        printf("%i %i\n",a[i].kezd,a[i].veg);
+//    }
+
+    //rendezzuk a filmeket a vegzodesukhoz kepest
+    FILM aux;
+    for(int i=0;i<n-1;++i){
+        for(int j=i+1;j<n;++j){
+            if(a[i].veg>a[j].veg){
+                aux=a[i];
+                a[i]=a[j];
+                a[j]=aux;
+            }
+        }
     }
 
+//    printf("A filmek rendezes utan:\n");
+//    for(int i=0;i<n;++i){
+//        printf("%s:",a[i].cim);
+//        printf("%i %i\n",a[i].kezd,a[i].veg);
+//    }
+
+    //Filmek kivalasztasa---Greedy
+    printf("Kivalasztott filmek:\n");
+    printf("%s: %i %i\n",a[0].cim,a[0].kezd,a[0].veg);
+    int megnezve=0;
+    for(int i=1;i<n;++i){
+        if(a[i].kezd>=a[megnezve].veg){
+            printf("%s: %i %i\n",a[i].cim,a[i].kezd,a[i].veg);
+            megnezve=i;
+        }
+    }
     return 0;
 }
